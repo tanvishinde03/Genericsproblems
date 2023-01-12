d1c9e477df47be1ab30c33fd894b116fa758fedc
tanvishinde03 <shindetanvi0203@gmail.com>
2023-01-12 20:44:43 +0530

    GenericsProblems

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using findmaximumnumber;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Genericsproblems
+{
+    internal class Program
+    {
+        public static object FindMaximum { get; private set; }
+
+        public  static void Main(string[] args)
+        {
+            
+            
+
+            Console.WriteLine("Welcome to the Generics Problem : ");
+            FindMaximum.FindMaxValue(20, 40, 50);
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ findmaximum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace findmaximumnumber
+{
+    internal class findmaximum
+    {
+        public static void FindMaxValue(int first, int second, int third)
+        {
+            if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("{0} is Maximum", first);
+            }
+            else if (second.CompareTo(first) >= 0 && second.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("{0} is  Maximum", second);
+            }
+            else if (third.CompareTo(first) >= 0 && third.CompareTo(second) >= 0)
+            {
+                Console.WriteLine("{0} is Maxm", third);
+            }
+            else
+                Console.WriteLine("All number are equal");
+        }
+    }
+
+
+
+
+
+}
+
