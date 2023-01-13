a9ed58f67528065f628be78ff1585d903140ebfc
tanvishinde03 <shindetanvi0203@gmail.com>
2023-01-13 16:20:41 +0530

    Addind All UC

--- Program.cs
+++ Program.cs
@@ -15,20 +15,43 @@ namespace Genericsproblems
         {
             Console.WriteLine("Welcome to the Generics Problem : ");
             findmaximum Number = new findmaximum();
-            findmaximum.FindMaxValue("zya", "zzz", "rew");
-            Console.ReadLine();
+            Console.WriteLine("Please choose the option in below");
+            Console.WriteLine("1:- Find Maximum Number\n" + "2:- Find Maximum Using Generics");
 
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FindMaximum.FindMaxValue(20, 40, 50);
+                        FindMaximum.FindMaxValue(20.5f, 10.3f, 11.3f);
+                        FindMaximum.FindMaxValue("zya", "zzz", "rew");
+                        break;
 
-      
+                    case 2:
+                        GenericsFindMax<int>.FindMaxValue(14, 23, 12);
+                        break;
 
-
-
-
-
-
-
-
+                    default:
+                        Console.WriteLine("Please choose the number given range");
+                        break;
+                }
+                Console.ReadLine();
+            }
 
         }
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
