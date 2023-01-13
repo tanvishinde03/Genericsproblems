82f43c6916841e6bc2ad89f2602416e5ff493923
tanvishinde03 <shindetanvi0203@gmail.com>
2023-01-13 18:09:03 +0530

    Implementing UC5

---
+++ GenericMaximum.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericsproblems
+{
+    public class GenericMaximum1
+    {
+        internal class GenericMaximum<T> where T : IComparable
+        {
+            public T[] value;
+            public GenericMaximum(T[] value)
+            {
+                this.value = value;
+            }
+            public T[] Sorting(T[] values)
+            {
+                Array.Sort(values);
+                return values;
+            }
+            public T MaxValue(params T[] values)
+            {
+                var sortedValues = Sorting(value);
+                return sortedValues[sortedValues.Length - 1];
+            }
+            public void PrintMaxValue()
+            {
+                var max = MaxValue(this.value);
+                Console.WriteLine("Maximum value is " + max);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
