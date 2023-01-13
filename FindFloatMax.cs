eec39eea13e2e71154c238ec145a5c5b91ce919e
tanvishinde03 <shindetanvi0203@gmail.com>
2023-01-13 15:34:28 +0530

    Finding Float Max Value

--- FindFloatMax.cs
+++ FindFloatMax.cs
@@ -8,5 +8,36 @@ namespace Genericsproblems
 {
     internal class FindFloatMax
     {
+        public static void FindMaxValue(float first, float second, float third)
+        {
+            if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("{0} is Maximum", first);
+            }
+            else if (second.CompareTo(first) >= 0 && second.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("{0} is  Maximum", second);
+            }
+            else if (third.CompareTo(first) >= 0 && third.CompareTo(second) >= 0)
+            {
+                Console.WriteLine("{0} is Maximum", third);
+            }
+            else
+                Console.WriteLine("All number are equal");
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
--- Program.cs
+++ Program.cs
@@ -10,17 +10,16 @@ namespace Genericsproblems
 {
     internal class Program
     {
-        public static object FindMaximum { get; private set; }
-
+     
         public  static void Main(string[] args)
         {
-            
-          
-           
             Console.WriteLine("Welcome to the Generics Problem : ");
-            FindMaximum Number = new findMaximum();
-            FindMaximum.FindMaxValue(20.5f, 10.3f, 11.3f);
+            findmaximum Number = new findmaximum();
+            findmaximum.FindMaxValue(20.5f, 10.3f, 11.3f);
             Console.ReadLine();
+
+
+      
 
 
 
